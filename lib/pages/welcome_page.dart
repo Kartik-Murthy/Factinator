1c4001d8c105a159c5bd93bc51f09afd7dff0460
Kartik-Murthy <kartik.spgk@gmail.com>
2023-04-19 17:14:02 +0530

    added welcome page

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,94 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:factinator/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:particles_flutter/particles_flutter.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue.shade900,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            CircularParticle(
+              key: UniqueKey(),
+              awayRadius: 80,
+              numberOfParticles: 20,
+              speedOfParticles: 2,
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              onTapAnimation: true,
+              particleColor: Colors.white.withAlpha(150),
+              awayAnimationDuration: const Duration(milliseconds: 1500),
+              maxParticleSize: 12,
+              isRandSize: true,
+              isRandomColor: false,
+
+              awayAnimationCurve: Curves.easeInOutBack,
+
+              connectDots: false, //not recommended
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      'F a c t i n a t o r',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    AnimatedTextKit(
+                        isRepeatingAnimation: false,
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                              'Fact your way to wisdom.\nQuote your way to inspiration.',
+                              textAlign: TextAlign.justify,
+                              textStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w500))
+                        ]),
+                    getStartedButton(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage()))),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getStartedButton({required Function()? onTap}) {
+    return GestureDetector(
+      onTap: () => onTap,
+      child: Container(
+        height: 52,
+        width: 160,
+        padding: const EdgeInsets.all(12),
+        decoration: ShapeDecoration(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+          shadows: kElevationToShadow[9],
+          color: Colors.white,
+        ),
+        child: const Text(
+          'Get Started  ➜',
+          style: TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
